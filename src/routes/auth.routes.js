7eacbeb18429650dea441d5f898397d928fa7b32
Fatima Khalid <khalidfatima364@gmail.com>
2026-08-30 23:16:08 -0700

    routes in place

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,12 +1,39 @@
 const express = require('express');
 const router = express.Router();
-const { authControllers: {handleSignup, handleVerifyCode, handleLogin, handleLogout, handleRefreshAccessToken} } = require('controllers');
-const { authMiddlewares: {validateSignup, validateCode, loginLimiter, validateLogin, validateLogout, validateRefreshAccessToken} } = require('middlewares');
+
+const { 
+    handleSignup,
+    handleResendVerificationCode, 
+    handleVerifyCode, 
+    handleLogin,
+    handleForgotPassword,
+    handleResetPassword,
+    handleChangePassword, 
+    handleLogout, 
+    handleRefreshAccessToken
+} = require('controllers/auth.controller');
+
+const { 
+    validateSignup,
+    validateResendVerificationCode,
+    validateCode, 
+    loginLimiter, 
+    validateLogin,
+    validateForgotPassword,
+    validateResetPassword,
+    validateCangePassword, 
+    validateRefreshAccessToken,
+    authenticateSession
+} = require('middlewares/auth.middleware');
 
 router.post('/signup', validateSignup, handleSignup);
+router.get('/resend-verification-code', validateResendVerificationCode, handleResendVerificationCode )
 router.post('/verify-code', validateCode, handleVerifyCode);
 router.post('/login', loginLimiter, validateLogin, handleLogin);
-router.post('/logout', validateLogout, handleLogout);
+router.post('/forgot-password', validateForgotPassword, handleForgotPassword);
+router.post('/reset-password', validateResetPassword, handleResetPassword);
+router.post('/change-password', authenticateSession, validateCangePassword, handleChangePassword  )
+router.post('/logout', authenticateSession, handleLogout);
 router.post('/refresh', validateRefreshAccessToken, handleRefreshAccessToken)
 
 
